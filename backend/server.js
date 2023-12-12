dc0703c9537597aa69e12d8f329c38db1e9f2ae2
santhosh210 <kondasanthosh210@gmail.com>
2023-12-12 16:16:43 +0530

    Initialized frontend and backend

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const notes = require("./data/notes");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config(app);
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/api/notes", (req, res) => res.send(notes));
+app.get("/api/notes/:id", (req, res) => {
+  const note = notes.find((n) => n._id === req.params.id);
+  res.send(note);
+});
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
