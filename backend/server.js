4e67fa4c87dc7bc8d79cab65dd4b816c180cfc5e
santhosh210 <kondasanthosh210@gmail.com>
2023-12-15 18:13:00 +0530

    added DB

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const notes = require("./data/notes");
 const dotenv = require("dotenv");
-const connectDB = require("./config/db");
 const app = express();
 dotenv.config(app);
-connectDB();
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
