b2ed563f3d9f444b6f8dd1291af194f484b5a5b3
santhosh210 <kondasanthosh210@gmail.com>
2023-12-15 18:01:08 +0530

    Added DB connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,8 @@
 const mongooose = require("mongoose");
 const connectDB = async () => {
   try {
-    const conn = await mongooose.connect(
-      "mongodb+srv://kondasanthosh210:aA1JcHceBrrws6XC@cluster0.uij5a6j.mongodb.net/?retryWrites=true&w=majority"
-    );
-    console.log(`MongoDB connection established:${conn}`);
+    const conn = await mongooose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB connection established:${conn.connection.host}`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
     process.exit();
