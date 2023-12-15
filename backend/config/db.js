1c48c1f6455dc850559dfce750879a3749aba872
santhosh210 <49731041+santhosh210@users.noreply.github.com>
2023-12-15 17:59:43 +0530

    Update db.js
    Remove MONGO_URI

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongooose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongooose.connect(
-      "mongodb+srv://kondasanthosh210:aA1JcHceBrrws6XC@cluster0.uij5a6j.mongodb.net/?retryWrites=true&w=majority"
+     process.env.MONGO_URI
     );
     console.log(`MongoDB connection established:${conn}`);
   } catch (error) {
