43364f1230e6312f3fa5354530f9aaccbc27da2d
santhosh210 <kondasanthosh210@gmail.com>
2023-12-16 15:35:28 +0530

    Added connect Db to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const notes = require("./data/notes");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 const app = express();
 dotenv.config(app);
 const PORT = process.env.PORT || 5000;
-
+connectDB();
 app.get("/", (req, res) => res.send("Hello World!"));
 app.get("/api/notes", (req, res) => res.send(notes));
 app.get("/api/notes/:id", (req, res) => {
